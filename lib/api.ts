1157bdaff25cd5786f57a05ff28d2103ba780612
Edan Harr <edan@analyticintelligencesolutions.com>
2025-11-15 13:52:49 -0800

    Create api.ts

---
+++ lib/api.ts
@@ -0,0 +1,12 @@
+// Placeholder file so imports succeed.
+// Later you can wire this up to your ingestion / agent servers.
+
+export async function fetchFlights() {
+  // TODO: call your Supabase / backend here
+  return [];
+}
+
+export async function fetchRerouteProposals() {
+  // TODO: call your agent server here
+  return [];
+}
